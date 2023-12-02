fa0b57bad1aacaf79cc08893fb3cb1609358ce78
abelbascu <abel.bascu@gmail.com>
2023-12-02 18:36:05 +0100

    Player class refactor
    The class was a mess, with many unnecessary public variables and it was difficult to follow the execution flow.
    The _UnhandledInput class was removed as the Updatedirection-GetDrirection methods were spilt between _UnhandledInput and _Process(), which seemed counterintuitive.

--- Assignment 3 - Player vs Enemies (Godot)/Player.cs
+++ Assignment 3 - Player vs Enemies (Godot)/Player.cs
@@ -4,86 +4,53 @@ using System.Collections;
 using System.Threading;
 using static Godot.TextServer;
 
-public partial class Player : CharacterBody2D
+public partial class Player : CharacterBody2D, IDamageable
 {
-    public const float Speed = 50;
+    public int Health { get; set; } = 10;
     public AnimatedSprite2D animatedSprite = new AnimatedSprite2D();
     public bool isAttacking;
-    public Vector2 direction;
-    public Vector2 new_direction;
-    public delegate void AnimationFinished();
-    AnimationFinished animationFinished;
-
+    public Action AnimationEnded;
 
 
     public override void _Ready()
     {
         animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        animationFinished += OnAnimationFinished;
+        // animatedSprite.AnimationFinished += OnAnimationFinished; //we can subscribe to signals by code instead of the inspector
+        AnimationEnded += OnAnimationFinished; //another way, using indirection to assign first the callback to a custom action
+        animatedSprite.AnimationFinished += AnimationEnded; //then subscribing the custom action to the inbuilt signal
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 velocity = Velocity;
-       
-        
-        //if (direction != Vector2.Zero)
-        //{
-            velocity.Y = direction.Y * Speed;
-            velocity.X = direction.X * Speed;
-        //}
+        UpdateDirection(GetDirection());
+        UpdateAttackStatus(GetDirection());
+    }
 
+    public Vector2 GetDirection()
+    {
+        Vector2 direction = Input.GetVector("walk_left", "walk_right", "walk_up", "walk_down");
+        return direction;
+    }
+
+    public void UpdateDirection(Vector2 direction)
+    {
+        float Speed = 50;
+
+        Vector2 velocity = Velocity; //Velocity is the internal property of the RigidBody2D
+        velocity.Y = direction.Y * Speed;
+        velocity.X = direction.X * Speed;
         Velocity = velocity;
         if (!isAttacking)
         {
             MoveAndSlide();
-            //Velocity = Vector2.Zero;
-            PlayerAnimations(direction);
+            UpdateDirectionAnimations(direction);
         }
     }
 
-    public override void _Input(InputEvent @event)
+    public void UpdateDirectionAnimations(Vector2 direction)
     {
-        string animationName;
-
-        direction = Input.GetVector("walk_left", "walk_right", "walk_up", "walk_down");
-
-        if (Input.IsActionPressed("attack"))
-        {
-            isAttacking = true;
-            animationName = "attack_" + ReturnedDirection(new_direction);
-            if(new_direction == Vector2.Right)
-                {
-                animatedSprite.FlipH = true;
-                }
-            animatedSprite.Play(animationName);
-            
-        }
-    }
-
-    
-    public void OnAnimationFinished()
-    {
-        isAttacking = false;
-        animatedSprite.FlipH = false;
-        //Callable.From
-    }
-
-    public void PlayerAnimations(Vector2 direction)
-    {
-        string animationName;
-
-        if (direction != Vector2.Zero)
-        {
-            new_direction = direction;
-            animationName = "walk_" + ReturnedDirection(new_direction);
-            animatedSprite.Play(animationName);
-        }
-        else
-        {
-            animationName = "idle";
-            animatedSprite.Play(animationName);
-        }
+        string animationName = (direction != Vector2.Zero) ?  "walk_" + ReturnedDirection(direction) : "idle" ;
+        animatedSprite.Play(animationName);
     }
 
     public string ReturnedDirection(Vector2 direction)
@@ -99,5 +66,36 @@ public partial class Player : CharacterBody2D
         if (normalizedDirection.X < 0)
             return "left";
         return "left";
+    }
+
+    public void UpdateAttackStatus(Vector2 direction)
+    {
+        ShowAttackAnimation(direction);
+    }
+
+    public void ShowAttackAnimation(Vector2 direction)
+    {
+        string animationName;
+        if (Input.IsActionPressed("attack"))
+        {
+            isAttacking = true;
+            animationName = "attack_" + ReturnedDirection(direction);
+            if (direction == Vector2.Right)
+            {
+                animatedSprite.FlipH = true; //we reuse attack_left animation
+            }
+            animatedSprite.Play(animationName);
+        }
+    }
+
+    public void OnAnimationFinished()
+    {
+        isAttacking = false;
+        animatedSprite.FlipH = false;
+    }
+
+    public void ReceiveDamage()
+    {
+
     }
 }
